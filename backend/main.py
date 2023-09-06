e7f41298c5e2d05f188d5f1e99579e64604e3461
Daniel Beilin <beylin.daniel@gmail.com>
2023-09-06 14:37:28 +0300

    ignore E501 error in linting process

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,18 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from backend.api import ses_templates
+import logging
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import load_dotenv
-import logging
+
 
 # Setting up logging
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    handlers=[logging.FileHandler("log/app.log"), logging.StreamHandler()])
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    handlers=[logging.FileHandler("log/app.log"), logging.StreamHandler()],
+)
 
 logger = logging.getLogger(__name__)
 app = FastAPI()
@@ -35,10 +38,15 @@ app.mount("/static", StaticFiles(directory="frontend"), name="frontend")
 # Include API routes
 app.include_router(ses_templates.router, prefix="/ses")
 
+
 @app.get("/")
 def serve_root():
-    return FileResponse('frontend/index.html')
+    return FileResponse("frontend/index.html")
+
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True)
+
+    uvicorn.run(
+        "main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8000)), reload=True
+    )
--- backend/utils/aws_utils.py
+++ backend/utils/aws_utils.py
@@ -3,13 +3,15 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 def create_ses_client():
     try:
-        client = boto3.client('ses')
+        client = boto3.client("ses")
         return client
     except Exception as e:
         logger.error(f"Failed to create SES client: {e}")
         raise
+
 
 def get_total_templates_count():
     try:
@@ -23,10 +25,10 @@ def get_total_templates_count():
             else:
                 response = client.list_templates()
 
-            count += len(response['TemplatesMetadata'])
+            count += len(response["TemplatesMetadata"])
 
             # If NextToken exists in the response, then there are more templates to fetch
-            next_token = response.get('NextToken', None)
+            next_token = response.get("NextToken", None)
             if not next_token:
                 break
 
@@ -35,60 +37,63 @@ def get_total_templates_count():
         logger.error(f"Failed to get total templates count: {e}")
         raise
 
+
 def list_ses_templates(page, size):
     try:
         client = create_ses_client()
-        
+
         # Fetch the total number of templates
         total_templates_count = get_total_templates_count()
 
         # Compute the total number of pages
         total_pages = (total_templates_count + size - 1) // size
 
-        params = {
-            'MaxItems': size
-        }
+        params = {"MaxItems": size}
         for _ in range(page):
             response = client.list_templates(**params)
-            
+
             # Check if there is a NextToken in the response. If there is, it means there are more pages.
-            if 'NextToken' in response:
-                params['NextToken'] = response['NextToken']
+            if "NextToken" in response:
+                params["NextToken"] = response["NextToken"]
             else:
                 if _ < page - 1:
                     return []
-        
-        return {
-            "templates": response['TemplatesMetadata'],
-            "totalPages": total_pages
-        }
+
+        return {"templates": response["TemplatesMetadata"], "totalPages": total_pages}
     except Exception as e:
-        logger.error(f"Failed to list SES templates for page {page} with size {size}: {e}")
+        logger.error(
+            f"Failed to list SES templates for page {page} with size {size}: {e}"
+        )
         raise
+
 
 def get_ses_template(template_name):
     try:
         client = create_ses_client()
         response = client.get_template(TemplateName=template_name)
-        return response['Template']
+        return response["Template"]
     except Exception as e:
         logger.error(f"Failed to get SES template {template_name}: {e}")
         raise
 
+
 def create_ses_template(template_data):
     try:
         client = create_ses_client()
-        
+
         # Ensure HtmlPart and TextPart are not None
-        template_data['HtmlPart'] = template_data.get('HtmlPart') or ""
-        template_data['TextPart'] = template_data.get('TextPart') or ""
-        
+        template_data["HtmlPart"] = template_data.get("HtmlPart") or ""
+        template_data["TextPart"] = template_data.get("TextPart") or ""
+
         response = client.create_template(Template=template_data)
         logger.info(f"Created SES template with name {template_data['TemplateName']}.")
         return response
     except Exception as e:
-        logger.error(f"Failed to create SES template with name {template_data['TemplateName']}: {e}")
+        logger.error(
+            f"Failed to create SES template with name {template_data['TemplateName']}: {e}"
+        )
         raise
+
 
 def update_ses_template(template_name, template_data):
     try:
@@ -99,6 +104,7 @@ def update_ses_template(template_name, template_data):
     except Exception as e:
         logger.error(f"Failed to update SES template {template_name}: {e}")
         raise
+
 
 def delete_ses_template(template_name):
     try:
